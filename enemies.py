53fc9563b25c578ce5bb3200b6421ab243f323ae
Khenya <abrendakhenya@gmail.com>
2023-08-20 15:14:39 -0400

    fantasma

--- enemies.py
+++ enemies.py
@@ -1,11 +1,16 @@
 import arcade
-
+WIDTH = 800
+HEIGHT = 600
+SPEED = 2
 class Enemies(arcade.Sprite):
     def __init__(self, image, scale, center_x, center_y):
-        super().__init__(image, scale, center_x=center_x, center_y=center_y)
-        self.change_x = 0
-        self.change_y = 0
-    
+        super().__init__(image, scale)
+        self.center_x = center_x
+        self.center_y = center_y
+        self.change_x = SPEED
+
     def update(self):
-        # TODO hacer su logica fantasamas
-        pass
+        self.center_x += self.change_x
+        if self.left < 0 or self.right > WIDTH:
+            self.change_x *= -1
+
